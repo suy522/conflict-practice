cbadadb236f46b1cc09bf88b5ffdf89b8b048bcb
suy <sugarry2846@gmail.com>
2025-11-14 09:50:04 +0900

    feat: print(fizzbuzz)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 for i in range(1,15+1):
-    if i%5==0 :
+    if i%15==0 :
+       print('fizzbuzz')
+    elif i%5==0 :
        print('buzz')
-    if i%3==0 :
+    elif i%3==0 :
        print('fizz')
     else:
        print(i)
